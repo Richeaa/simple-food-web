f04e9560e9f03d00f55703f199d22ab8c9d06ff7
Rich <rich.anthonios@student.president.ac.id>
2024-06-28 14:24:17 +0700

    first init github

--- mis.js
+++ mis.js
@@ -1,15 +1,17 @@
-let swiperCards = new Swiper(".inti2", {
-    loop: true,
-    spaceBetween: 32,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    
+document.addEventListener('DOMContentLoaded', function () {
+    let swiperCards = new Swiper(".inti2", {
+        loop: true,
+        spaceBetween: 32,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 3,
+    });
 });
